83d58227f062d407ab7a2b8f99726f7772116c82
mark churcher <markchurcher@gmail.com>
2021-06-12 16:54:04 +1200

    initial commit

---
+++ setup-my-mac.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+xcode-select --install
+# install home-brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+brew install --cask visual-studio-code
+brew install --cask brave-browser
+brew install --cask docker
+brew install --cask ableton-live-suite
+
+brew install --cask vlc 
