b2320e8336a02f4cfa83ade6ae6b8dbb30fd2c24
mark churcher <markchurcher@gmail.com>
2021-06-20 14:41:48 +1200

    maint: group into categories

--- setup-my-mac.sh
+++ setup-my-mac.sh
@@ -3,16 +3,20 @@ xcode-select --install
 # install home-brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-
-brew install --cask visual-studio-code
+# basics
 brew install --cask brave-browser
-brew install --cask docker
+brew install --cask spotify
+
+
+# dev
 brew install gh
+brew install --cask visual-studio-code
+brew install --cask docker
+
+# music
 brew install --cask ableton-live-suite
 
+# media
 brew install --cask vlc 
-
-
 brew install --cask transmission 
 brew install --cask handbrake
-brew install --cask spotify
